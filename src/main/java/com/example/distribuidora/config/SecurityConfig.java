a73a51ee1fc89e85eda6baee6fe5017e2ff8afc3
facuzambellini5 <facundozambellini@gmail.com>
2026-07-13 11:51:40 -0300

    config inicial de spring security y agregado de dependencia para documentación

---
+++ src/main/java/com/example/distribuidora/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.distribuidora.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity // Habilita @PreAuthorize, @PostAuthorize, etc.
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/customer/**").permitAll()
+                        .anyRequest().authenticated());
+
+                //TODO VER
+               //.formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
